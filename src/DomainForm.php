a0c21b6dee112ed12ae7080640498b99c62e4e8b
Philippe von Bergen <ph.vonbvergen@iqual.ch>
2020-08-14 14:04:59 +0200

    Extend default and domain form, integrate hooks

--- src/DomainForm.php
+++ src/DomainForm.php
@@ -15,23 +15,84 @@ class DomainForm extends OrigForm {
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
-    $form['validate_url']['#default_value'] = FALSE;
-    $form['validate_url']['#type'] = 'hidden';
+    $user = \Drupal::currentUser();
+    $baseConfig = $this->config('iq_multidomain_extensions.rancher_settings');
+    $moduleHandler = \Drupal::service('module_handler');
+    $domainThemeSwitch = $moduleHandler->moduleExists('domain_theme_switch');
+    $stylingProfileThemeSwitch = $moduleHandler->moduleExists('styling_profiles_domain_switch');
 
-    // $form['hostname']['#type'] ='hidden';
-    $form['hostname']['#field_suffix'] = '.' . getenv('DOMAIN_BASE');
-    // $form['hostname']['#default_value'] = '';
-    $form['hostname']['#default_value'] = str_replace('.' . getenv('DOMAIN_BASE'), '', $form['hostname']['#default_value']);
-    // $form['id']['#type'] = 'hidden';
-    /*$form['url_prefix'] = array(
-    '#type' => 'textfield',
-    '#title' => $this->t('URL Prefix'),
-    '#size' => 40,
-    '#maxlength' => 80,
-    '#default_value' => $this->entity->getThirdPartySetting('iq_multidomain_extensions', 'url_prefix', ''),
-    '#description' => $this->t('URL Prefix for specific domain. Leave empty for main domain'),
-    );*/
+    $form['extensions'] = [
+      '#type' => 'details',
+      '#title' => $this->t('iqual extensions'),
+      '#description' => $this->t('These settings enhance the domain creation. Default values can be set under /de/admin/config/system/iqual_domain/configuration'),
+      '#open' => TRUE,
+    ];
+    $form['extensions']['url_prefix'] = [
+      '#type' => 'textfield',
+      '#required' => TRUE,
+      '#title' => $this->t('URL prefix'),
+      '#description' => $this->t('The url prefix for aliases for this domain.'),
+      '#default_value' => $this->entity->getThirdPartySetting('iq_multidomain_extensions', 'url_prefix'),
+    ];
+$form['validate_url']['#default_value'] = FALSE;
 
+    if ($this->entity->isNew()) {
+
+      $form['extensions']['create_menu'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Create menu.'),
+        '#description' => $this->t('Whether to automatically create a menu for the new domain.'),
+        '#default_value' => $baseConfig->get('create_menu'),
+      ];
+      $contentTypes = \Drupal::service('entity.manager')->getStorage('node_type')->loadMultiple();
+      $contentTypesList = [];
+      foreach ($contentTypes as $contentType) {
+        $contentTypesList[$contentType->id()] = $contentType->label();
+      }
+      $form['extensions']['menu_content_types'] = [
+        '#type' => 'checkboxes',
+        '#title' => $this->t('Content types for new menu.'),
+        '#description' => $this->t('Content types for new menu.'),
+        '#options' => $contentTypesList,
+        '#default_value' => $baseConfig->get('menu_content_types'),
+      ];
+
+      if ($domainThemeSwitch) {
+        $form['extensions']['copy_theme'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Copy the base theme'),
+          '#description' => $this->t('Whether to automatically copy the base theme on creating a new domain entry.'),
+          '#default_value' => $baseConfig->get('copy_theme'),
+          '#disabled' => !$domainThemeSwitch,
+        ];
+      }
+      if ($stylingProfileThemeSwitch) {
+        $form['extensions']['create_styling_profile'] = [
+          '#type' => 'checkbox',
+          '#title' => $this->t('Create styling profile'),
+          '#description' => $this->t('Whether to automatically create a styling profile with each new domain.'),
+          '#default_value' => $baseConfig->get('create_styling_profile'),
+        ];
+      }
+      $form['#isnew'] = TRUE;
+      $form['hostname']['#field_suffix'] = '.' . getenv('DOMAIN_BASE');
+      $form['hostname']['#default_value'] = str_replace('.' . getenv('DOMAIN_BASE'), '', $form['hostname']['#default_value']);
+      $form['hostname']['#default_value'] = '';
+      $form['name']['#default_value'] = '';
+    }
+    else {
+      $form['#isnew'] = FALSE;
+    }
+    if (!$user->hasPermission('administer iq_multidomain_extensions domains')) {
+
+      $form['validate_url']['#type'] = 'hidden';
+      $form['status']['#type'] = 'hidden';
+      $form['scheme']['#type'] = 'hidden';
+      $form['is_default']['#type'] = 'hidden';
+      if (!$this->entity->isNew()) {
+        $form['hostname']['#disabled'] = TRUE;
+      }
+    }
     return $form;
   }
 
@@ -39,22 +100,31 @@ class DomainForm extends OrigForm {
    *
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $entity = $this->entity;
-    $length = strlen($form['hostname']['#value'] . '.' . getenv('DOMAIN_BASE'));
-    $entity->setHostname(substr($entity->getHostname() . $form['hostname']['#field_suffix'], 0, $length));
-    $form['hostname']['#value'] = $entity->getHostname();
-    $form_state->setValue('hostname', $form['hostname']['#value']);
-    // TODO: Change the autogenerated stub.
+    $user = \Drupal::currentUser();
+    if (!$user->hasPermission('administer iq_multidomain_extensions domains')) {
+      if ($this->entity->isNew()) {
+        $form_state->setValue('hostname', $form_state->getValue('hostname') . '.' . getenv('DOMAIN_BASE'));
+        $this->entity->setHostname($form_state->getValue('hostname'));
+      }
+    }
     parent::validateForm($form, $form_state);
   }
 
   /**
    *
    */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::service('iq_multidomain_extensions.service.domain')->processDomainForm($form, $form_state);
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   *
+   */
   public function save(array $form, FormStateInterface $form_state) {
+    $this->entity->setThirdPartySetting('iq_multidomain_extensions', 'url_prefix', $form_state->getValue('url_prefix'));
     parent::save($form, $form_state);
-    // $this->entity->setThirdPartySetting('iq_multidomain_extensions', 'url_prefix', $form_state->getValue('url_prefix'));
-    $form_state->setRedirect('iq_multidomain_extensions.settings');
+    $user = \Drupal::currentUser();
   }
 
 }
--- src/Form/RancherForm.php
+++ src/Form/RancherForm.php
@@ -24,30 +24,106 @@ class RancherForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('iq_multidomain_extensions.rancher_settings');
+    $moduleHandler = \Drupal::service('module_handler');
 
     $base_theme = $config->get('base_theme');
     $directory_path = $config->get('directory_path');
     $rancher_endpoint = $config->get('rancher_endpoint');
+    $copy_theme = $config->get('copy_theme');
+    $domainThemeSwitch = $moduleHandler->moduleExists('domain_theme_switch');
+    $domainAccess = $moduleHandler->moduleExists('domain_access');
+    $stylingProfileThemeSwitch = $moduleHandler->moduleExists('styling_profiles_domain_switch');
 
-    $form['directory_path'] = [
-      '#type' => 'textfield',
-      '#title' => 'Directory path',
-      '#description' => $this->t('Directory path to the base theme. Example: themes/contrib'),
-      '#default_value' => isset($directory_path) ? $directory_path : '',
-    ];
-
-    $form['base_theme'] = [
-      '#type' => 'textfield',
-      '#title' => 'Base theme',
-      '#description' => $this->t('Name of the base theme to be copied.'),
-      '#default_value' => isset($base_theme) ? $base_theme : '',
-    ];
     $form['rancher_endpoint'] = [
       '#type' => 'textfield',
       '#title' => 'Rancher endpoint',
       '#description' => $this->t('The rancher api endpoint.'),
       '#default_value' => isset($rancher_endpoint) ? $rancher_endpoint : '',
     ];
+
+    $form['notification_email'] = [
+      '#type' => 'email',
+      '#title' => 'Notification email',
+      '#description' => $this->t('The email address to notify about a new domain record.'),
+      '#default_value' => !empty($config->get('notification_email')) ? $config->get('notification_email') : '',
+    ];
+
+    $form['menu'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Menu'),
+      '#description' => $this->t('This section allows to preset the menu settings.'),
+      '#open' => true
+    ];
+
+    $form['menu']['create_menu'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Create a menu per domain.'),
+      '#description' => $this->t('Whether to automatically create a menu for a new domain entry.'),
+      '#default_value' => !empty($config->get('create_menu')) ? $config->get('create_menu') : 0,
+    ];
+    $contentTypes = \Drupal::service('entity.manager')->getStorage('node_type')->loadMultiple();
+    $contentTypesList = [];
+    foreach ($contentTypes as $contentType) {
+      $contentTypesList[$contentType->id()] = $contentType->label();
+    }
+    $form['menu']['menu_content_types'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Content types for new menu.'),
+      '#description' => $this->t('Content types for new menu.'),
+      '#options' => $contentTypesList,
+      '#default_value' => $config->get('menu_content_types'),
+    ];
+
+    $form['menu']['create_menu'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Create a menu per domain.'),
+      '#description' => $this->t('Whether to automatically create a menu for a new domain entry.'),
+      '#default_value' => !empty($config->get('create_menu')) ? $config->get('create_menu') : 0,
+    ];
+
+    $form['styling'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Design'),
+      '#description' => $this->t('This section allows to automate design and styling when creating a new domain.'),
+    ];
+    $form['styling']['create_styling_profile'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Create styling profile'),
+      '#description' => $this->t('Whether to automatically create a styling profile with each new domain.')   . ((!$stylingProfileThemeSwitch) ? '<br />' . $this->t('Only available if styling_profiles_domain_switch is installed.') : ''),
+      '#default_value' => $config->get('create_styling_profile'),
+      '#disabled' => !$stylingProfileThemeSwitch,
+    ];
+
+    // $form['styling'] = [
+    //   '#type' => 'details',
+    //   '#title' => $this->t('Theme settings'),
+    //   '#description' => $this->t('These settings allow to automatically copy a base theme with each new domain. Only available if domain_theme_switch is installed.'),
+
+    // ];
+    $form['styling']['copy_theme'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Copy the base theme'),
+      '#description' => $this->t('Whether to automatically copy the base theme on creating a new domain entry.')  . ((!$domainThemeSwitch) ? '<br />' . $this->t('Only available if domain_theme_switch is installed.') : ''),
+      '#default_value' => isset($copy_theme) ? $copy_theme : 0,
+      '#disabled' => !$domainThemeSwitch,
+    ];
+
+    $form['styling']['directory_path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Theme directory'),
+      '#description' => $this->t('Path to the base theme, e.g. "themes/contrib".') . ((!$domainThemeSwitch) ? '<br />' . $this->t('Only available if domain_theme_switch is installed.') : ''),
+      '#default_value' => isset($directory_path) ? $directory_path : '',
+      '#disabled' => !$domainThemeSwitch,
+    ];
+
+    $form['styling']['base_theme'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Base theme'),
+      '#description' => $this->t('Machine name of the base theme to be copied.')  . ((!$domainThemeSwitch) ? '<br />' . $this->t('Only available if domain_theme_switch is installed.') : ''),
+      '#default_value' => isset($base_theme) ? $base_theme : '',
+      '#disabled' => !$domainThemeSwitch,
+    ];
+
 
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = [
@@ -67,6 +143,11 @@ class RancherForm extends ConfigFormBase {
       ->set('directory_path', $form_state->getValue('directory_path'))
       ->set('base_theme', $form_state->getValue('base_theme'))
       ->set('rancher_endpoint', $form_state->getValue('rancher_endpoint'))
+      ->set('copy_theme', $form_state->getValue('copy_theme'))
+      ->set('create_menu', $form_state->getValue('create_menu'))
+      ->set('notification_email', $form_state->getValue('notification_email'))
+      ->set('menu_content_types', $form_state->getValue('menu_content_types'))
+      ->set('create_styling_profile', $form_state->getValue('create_styling_profile'))
       ->save();
     parent::submitForm($form, $form_state);
   }
