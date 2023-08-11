0fd96a6b99ba6035cc41db0f9c59f64f4629edc1
Philippe von Bergen <ph.vonbergen@iqual.ch>
2023-08-11 12:48:44 +0000

    Create sub module to handle patterns with domain_theme_switch

---
+++ modules/iq_multidomain_domain_theme_switch/src/Plugin/UiPatterns/Pattern/MultiDomainLibraryPattern.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\iq_multidomain_domain_theme_switch\Plugin\UiPatterns\Pattern;
+
+use Drupal\ui_patterns_library\Plugin\UiPatterns\Pattern\LibraryPattern;
+
+/**
+ * The UI Pattern plugin.
+ *
+ * ID is set to "yaml" for backward compatibility reasons.
+ *
+ * @UiPattern(
+ *   id = "MultiDomainLibraryPattern",
+ *   label = @Translation("MultiDomain Library Pattern"),
+ *   description = @Translation("Pattern defined using a YAML file."),
+ *   deriver = "\Drupal\iq_multidomain_extensions\Plugin\Deriver\MultiDomainLibraryDeriver"
+ * )
+ */
+class MultiDomainLibraryPattern extends LibraryPattern {
+
+}
---
+++ modules/iq_multidomain_domain_theme_switch/src/Plugin/views/row/Pattern.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\iq_multidomain_domain_theme_switch\Plugin\views\row;
+
+use Drupal\iq_multidomain_extensions\Form\PatternDisplayFormTrait;
+use Drupal\ui_patterns_views\Plugin\views\row\Pattern as OriginalPattern;
+
+/**
+ * Overwrites default row plugin.
+ */
+class Pattern extends OriginalPattern {
+
+  use PatternDisplayFormTrait;
+
+}
