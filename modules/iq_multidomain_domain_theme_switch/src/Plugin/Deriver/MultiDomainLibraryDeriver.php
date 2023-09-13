2fb70cb0288202b926513c19e7237e8ede98d8f6
Philippe von Bergen <ph.vonbergen@iqual.ch>
2023-09-13 12:47:26 +0000

    Rewrite deriver to use domain config settings

--- modules/iq_multidomain_domain_theme_switch/src/Plugin/Deriver/MultiDomainLibraryDeriver.php
+++ modules/iq_multidomain_domain_theme_switch/src/Plugin/Deriver/MultiDomainLibraryDeriver.php
@@ -16,15 +16,16 @@ class MultiDomainLibraryDeriver extends LibraryDeriver {
    */
   public function getPatterns() {
     $patterns = parent::getPatterns();
-
-    // Collect all secondary themes to prefix the pattern ids.
-    $domain_storage = \Drupal::service('entity_type.manager')->getStorage('domain');
-    if ($domain_theme_switch_config = \Drupal::config('domain_theme_switch.settings')) {
+    $prefix_themes = [];
+    $domain_config_settings = \Drupal::config('domain_config_ui.settings');
+    if (strpos($domain_config_settings->get('path_pages'), '/admin/appearance') !== FALSE) {
+      // Collect all secondary themes to prefix the pattern ids.
+      $domain_storage = \Drupal::service('entity_type.manager')->getStorage('domain');
       /** @var \Drupal\domain\Entity\Domain $domain */
       foreach ($domain_storage->loadMultipleSorted() as $domain) {
-        $theme_name = (string) $domain_theme_switch_config->get($domain->id() . '_site');
-        if (!$domain->isDefault() && !empty($theme_name)) {
-          $prefix_themes[] = $theme_name;
+        $domain_config = \Drupal::config('domain.config.' . $domain->id() . '.system.theme');
+        if (!$domain->isDefault() && !empty($domain_config->get('default'))) {
+          $prefix_themes[] = $domain_config->get('default');
         }
       }
     }
